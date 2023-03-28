63d2d06e4e0d51beecb069b43cc9e863dab67c29
Mikkel <Mikkel123sej@gmail.com>
2023-03-28 23:27:09 +0200

    used faster-than-lime solution

--- aoc2022/src/day_5.rs
+++ aoc2022/src/day_5.rs
@@ -1,274 +1,161 @@
+use core::fmt;
+
 use crate::Runnable;
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::HashMap;
-use std::fmt::Debug;
+
+use itertools::Itertools;
+#[allow(unused_imports)] // todo: remove
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_while1},
+    combinator::{all_consuming, map, map_res, opt},
+    sequence::{delimited, preceded, tuple},
+    Finish, IResult,
+};
 
 pub struct Solution;
 impl Runnable for Solution {
     fn run_with_input(&self, input: String) {
-        println!(
-            "Crates on top of each stack after moves: {}",
-            part_1_solve(input.as_str())
-        );
-    }
-}
-impl CrateStack {
-    #[allow(dead_code)]
-    fn new() -> Self {
-        CrateStack { crates: Vec::new() }
+        // println!(
+            // "Crates on top of each stack after moves: {}",
+            part_1_solve(input.as_str());
+        // );
     }
 }
 
-#[derive(Clone, Debug, PartialEq)]
-struct CrateStack {
-    crates: Vec<Crate>,
+#[derive(Clone, Copy)]
+struct Crate(char);
+impl fmt::Debug for Crate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
-impl From<&str> for CrateStack {
-    fn from(value: &str) -> Self {
-        CrateStack {
-            crates: value.chars().map(Crate::from).collect(),
+impl fmt::Display for Crate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+fn parse_crate(i: &str) -> IResult<&str, Crate> {
+    let first_char = |s: &str| Crate(s.chars().next().unwrap());
+    let f = delimited(tag("["), take(1_usize), tag("]"));
+    map(f, first_char)(i)
+}
+fn parse_hole(i: &str) -> IResult<&str, ()> {
+    map(tag("   "), drop)(i)
+}
+fn parse_crate_or_hole(i: &str) -> IResult<&str, Option<Crate>> {
+    alt((map(parse_crate, Some), map(parse_hole, |_| None)))(i) // parse for crate, then try hole, otherwise fail
+}
+fn parse_crate_line(i: &str) -> IResult<&str, Vec<Option<Crate>>> {
+    let (mut i, c) = parse_crate_or_hole(i)?;
+    let mut v = vec![c];
+
+    loop {
+        let (next_i, maybe_c) = opt(preceded(tag(" "), parse_crate_or_hole))(i)?;
+        match maybe_c {
+            Some(c) => v.push(c),
+            None => break,
+        }
+        i = next_i;
+    }
+
+    Ok((i, v))
+}
+
+struct Piles(Vec<Vec<Crate>>);
+impl fmt::Debug for Piles {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (i, pile) in self.0.iter().enumerate() {
+            writeln!(f, "Pile {}: {:?}", i, pile)?;
+        }
+        Ok(())
+    }
+}
+impl Piles {
+    #[allow(unused)]
+    fn apply_1(&mut self, ins: Instruction) {
+        for _ in 0..ins.quantity {
+            let el = self.0[ins.src].pop().unwrap();
+            self.0[ins.dst].push(el);
+        }
+    }
+    fn apply_2(&mut self, ins: Instruction) {
+        for krate in (0..ins.quantity)
+            .map(|_| self.0[ins.src].pop().unwrap())
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev()
+        {
+            self.0[ins.dst].push(krate);
         }
     }
 }
 
-#[derive(PartialEq, Copy, Clone, Debug)]
-struct Crate {
-    label: char,
+fn parse_number(i: &str) -> IResult<&str, usize> {
+    map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
+        s.parse::<usize>()
+    })(i)
 }
-
-impl From<char> for Crate {
-    fn from(label: char) -> Crate {
-        Crate { label }
-    }
-}
-impl TryFrom<&str> for Crate {
-    type Error = &'static str;
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        lazy_static! {
-            static ref RE_LABEL: Regex = Regex::new(r"[A-Z]").unwrap();
-        }
-
-        let label = RE_LABEL
-            .find(s)
-            .ok_or("Could not find a label in string literal.")?
-            .as_str();
-        let label: char = label.chars().next().ok_or("Label was not a char.")?;
-        Ok(Crate { label })
-    }
-}
-
-fn add_crate_line_to_stacks(stacks: &mut HashMap<usize, CrateStack>, line: &str) {
-    lazy_static! {
-        static ref RE_CRATE_PATTERN: Regex = Regex::new(r"    |\[[A-Z]\]").unwrap();
-    }
-
-    let line_crates: Vec<Option<Crate>> = RE_CRATE_PATTERN
-        .find_iter(line)
-        .map(|c| Crate::try_from(c.as_str()).ok())
-        .collect();
-    for crate_key in 1..=line_crates.len() {
-        let option_crate = line_crates[crate_key - 1]; // copy
-        if let Some(c) = option_crate {
-            stacks.entry(crate_key).or_insert_with(CrateStack::new);
-            stacks.get_mut(&crate_key).unwrap().crates.push(c);
-        }
-    }
-    println!("Finished line!");
+fn parse_pile_number(i: &str) -> IResult<&str, usize> {
+    map(parse_number, |i| i - 1)(i) // convert from 1-indexed to 0-indexed
 }
 
 #[derive(Debug)]
-struct Move {
-    count: usize,
-    source: usize,
-    target: usize,
+struct Instruction {
+    quantity: usize,
+    src: usize,
+    dst: usize,
 }
-impl TryFrom<&str> for Move {
-    type Error = &'static str;
+fn parse_instruction(i: &str) -> IResult<&str, Instruction> {
+    map(
+        tuple((
+            preceded(tag("move "), parse_number),
+            preceded(tag(" from "), parse_pile_number),
+            preceded(tag(" to "), parse_pile_number),
+        )),
+        |(quantity, src, dst)| Instruction { quantity, src, dst },
+    )(i)
+}
 
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        lazy_static! {
-            static ref RE_COUNT: Regex = Regex::new(r"move [0-99]").unwrap(); // replace with unspecific number?
-            static ref RE_FROM_INDEX: Regex = Regex::new(r"from [0-99]").unwrap(); // replace with unspecific number?
-            static ref RE_TO_INDEX: Regex = Regex::new(r"to [0-99]").unwrap(); // replace with unspecific number?
-        }
-        let from = RE_FROM_INDEX.find(s).ok_or("Missing segment: 'from'")?.as_str();
-        let from: usize = from.split_once(' ').unwrap().1.parse().unwrap();
-        let count = RE_COUNT.find(s).ok_or("Missing segment: 'count'")?.as_str();
-        let count: usize = count.split_once(' ').unwrap().1.parse().unwrap();
-        let to = RE_TO_INDEX.find(s).ok_or("Missing segmnet: 'to'")?.as_str();
-        let to: usize = to.split_once(' ').unwrap().1.parse().unwrap();
-        Ok(Move {
-            source: from,
-            count,
-            target: to,
+fn part_1_solve(input: &str) {
+    let mut lines = input.lines();
+
+    let crate_lines: Vec<_> = (&mut lines)
+        .map_while(|line| {
+            all_consuming(parse_crate_line)(line)
+                .finish()
+                .ok()
+                .map(|(_, line)| line)
         })
-    }
-}
+        .collect();
+    let mut piles = Piles(transpose_rev(crate_lines));
+    println!("{piles:?}");
 
-fn remove_crate_from_stack(stack: &mut CrateStack) -> Crate {
-    stack.crates.remove(stack.crates.len() - 1)
-}
-fn process_move_on_stacks(stacks: &mut HashMap<usize, CrateStack>, mut move_to_process: Move) {
-    // build move
-    let max_crates_to_take = stacks.get(&move_to_process.source).unwrap().crates.len();
-    if max_crates_to_take < move_to_process.count {
-        move_to_process.count = max_crates_to_take;
+    assert!(lines.next().unwrap().is_empty());
+    
+    for ins in lines.map(|line| all_consuming(parse_instruction)(line).finish().unwrap().1) {
+        println!("{ins:?}");
+        piles.apply_2(ins);
+        println!("{piles:?}");
     }
-    // use move
-    let mut crates_taken: Vec<Crate> = Vec::new();
-    let from_stack = stacks.get_mut(&move_to_process.source).unwrap();
+
     println!(
-        "Stack {} before move: {:?}",
-        &move_to_process.source, &from_stack.crates
+        "answer = {}",
+        piles.0.iter().map(|pile| pile.last().unwrap()).join("")
     );
-    for _ in 0..move_to_process.count {
-        if !from_stack.crates.is_empty() {
-            crates_taken.push(remove_crate_from_stack(from_stack));
-        }
-    }
-    println!(
-        "Moved {} crates from stack {} to stack {}: {:?}",
-        &move_to_process.count, &move_to_process.source, &move_to_process.target, &crates_taken
-    );
-
-    let to_stack = stacks.get_mut(&move_to_process.target).unwrap();
-    for crate_taken in crates_taken {
-        to_stack.crates.push(crate_taken);
-    }
 }
 
-fn part_1_solve(input: &str) -> String {
-    let parts = match input.split_once("\r\n\r\n") {
-        Some(p) => p, // string literal from .txt file
-        None => input.split_once("\n\n").unwrap(), // string literal in .rs file
-    };
-
-    // create starting condition
-    let mut stacks: HashMap<usize, CrateStack> = HashMap::new();
-    for line_of_crates in parts.0.lines().rev() {
-        add_crate_line_to_stacks(&mut stacks, line_of_crates);
-    }
-
-    // operate on state using moves (lines)
-    for line_of_move in parts.1.lines() {
-        let new_move = Move::try_from(line_of_move).expect("Invalid line for move");
-        process_move_on_stacks(&mut stacks, new_move);
-    }
-
-    // print the top crates of each stack (skip over empty)
-    get_top_labels(&stacks)
-}
-
-fn get_top_labels(stacks: &HashMap<usize, CrateStack>) -> String {
-    let mut top: String = String::new();
-    for stack_index in 1..=stacks.keys().len() {
-        let stack = stacks.get(&stack_index).unwrap();
-        let top_crate = stack.crates.last();
-        if let Some(top_crate) = top_crate {
-            top.push(top_crate.label);
-        };
-    }
-    top
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    const TEST_INPUT: &str = "    [D]
-[N] [C]
-[Z] [M] [P]
- 1   2   3
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2";
-    #[test]
-    fn part_1_example_works() {
-        assert_eq!("CMZ", part_1_solve(TEST_INPUT))
-    }
-    #[test]
-    fn creates_empty_crate() {
-        assert_eq!(None, Crate::try_from("   ").ok());
-    }
-    #[test]
-    fn creates_real_crate() {
-        assert_eq!(Some(Crate::from('C')), Crate::try_from("[C]").ok());
-    }
-    #[test]
-    fn adds_crates_properly() {
-        let line = "        [T]";
-        let mut stacks: HashMap<usize, CrateStack> = HashMap::from([
-            (1, CrateStack::from("CF")),
-            (2, CrateStack::from("L")),
-            (3, CrateStack::from("AQ")),
-        ]);
-        add_crate_line_to_stacks(&mut stacks, line);
-        let expected_stacks: HashMap<usize, CrateStack> = HashMap::from([
-            (1, CrateStack::from("CF")),
-            (2, CrateStack::from("L")),
-            (3, CrateStack::from("AQT")),
-        ]);
-        assert_eq!(&expected_stacks, &stacks);
-    }
-    #[test]
-    fn crates_move_correctly() {
-        let stacks_source: HashMap<usize, CrateStack> = HashMap::from([
-            (1, CrateStack::from("CF")),
-            (2, CrateStack::from("L")),
-            (3, CrateStack::from("A")),
-        ]);
-
-        // too big for stack
-        let big_move = Move {
-            count: 10,
-            source: 1,
-            target: 3,
-        };
-        let mut big_move_stacks = stacks_source.clone();
-        process_move_on_stacks(&mut big_move_stacks, big_move);
-        let big_move_expectation: HashMap<usize, CrateStack> = HashMap::from([
-            (1, CrateStack::new()),
-            (2, CrateStack::from("L")),
-            (3, CrateStack::from("AFC")),
-        ]);
-        assert_eq!(&big_move_expectation, &big_move_stacks);
-
-        // smaller than stack
-        let small_move = Move {
-            count: 1,
-            source: 1,
-            target: 3,
-        };
-        let mut small_move_stacks = stacks_source.clone();
-        process_move_on_stacks(&mut small_move_stacks, small_move);
-        let small_move_expectation: HashMap<usize, CrateStack> = HashMap::from([
-            (1, CrateStack::from("C")),
-            (2, CrateStack::from("L")),
-            (3, CrateStack::from("AF")),
-        ]);
-        assert_eq!(&small_move_expectation, &small_move_stacks);
-
-        // move doesn't take crates (count is 0)
-        let empty_move = Move {
-            count: 0,
-            source: 1,
-            target: 3,
-        };
-        let mut empty_move_stacks = stacks_source.clone();
-        process_move_on_stacks(&mut empty_move_stacks, empty_move);
-        let empty_move_expectation: HashMap<usize, CrateStack> = stacks_source; // we expect that nothing changes
-        assert_eq!(&empty_move_expectation, &empty_move_stacks);
-    }
-    #[test]
-    fn top_is_correct() {
-        let stacks: HashMap<usize, CrateStack> = HashMap::from([
-            (1, CrateStack::from("HL")),
-            (2, CrateStack::new()),
-            (3, CrateStack::from("AF")),
-            (4, CrateStack::from("SIQ")),
-        ]);
-        assert_eq!("LFQ".to_string(), get_top_labels(&stacks))
-    }
+fn transpose_rev<T>(v: Vec<Vec<Option<T>>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .rev()
+                .filter_map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
 }
