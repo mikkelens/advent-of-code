502bf1a4c6c2cd8043d25c8fac7f5923c0aa27b4
Mikkel <Mikkel123sej@gmail.com>
2023-04-03 20:37:44 +0200

    worked slightly on day 9 while configuring vim setup

--- aoc2022/src/day_9.rs
+++ aoc2022/src/day_9.rs
@@ -8,6 +8,7 @@ impl Runnable for Solution {
     }
 }
 
+use egui::{Color32, Sense, Stroke};
 use nom::{combinator::all_consuming, Finish};
 use parse::Instruction;
 
@@ -30,8 +31,15 @@ fn part_1_solve(input: &str) -> usize {
     eframe::run_native(
         "AoC 2022 - Day 9",
         options,
-        Box::new(|_cc| Box::new(MyApp { instructions }))
-    ).unwrap();
+        Box::new(|_cc| {
+            Box::new(MyApp {
+                instructions,
+                head: GridPos { x: 0, y: 0 },
+                tail: GridPos { x: 1, y: 1 },
+            })
+        }),
+    )
+    .unwrap();
 
     // todo!("answer part 1");
     0
@@ -57,8 +65,45 @@ impl eframe::App for MyApp {
             }
         });
         egui::CentralPanel::default().show(ctx, |ui| {
-            
+            let painter_size = egui::vec2(250.0, 250.0);
+            let (res, painter) = ui.allocate_painter(painter_size, Sense::hover());
+            let center = res.rect.center().to_vec2();
+
+            const SIDE: f32 = 16.0;
+            let to_panel_pos = |pos: GridPos| {
+                (egui::vec2(pos.x as f32 * SIDE, pos.y as f32 * SIDE) + center).to_pos2()
+            };
+
+            for x in -30..30 {
+                for y in -20..20 {
+                    let dot = GridPos { x, y };
+                    let is_zero = dot.x == 0 && dot.y == 0;
+
+                    let color = if is_zero {
+                        Color32::DARK_RED
+                    } else {
+                        Color32::LIGHT_GRAY
+                    };
+                    painter.circle_stroke(to_panel_pos(dot), 1.0, Stroke::new(1.0, color));
+                }
+            }
+
+            // paint the head
+            let head_pos = to_panel_pos(self.head);
+            painter.circle_stroke(head_pos, 6.0 / 2.0, Stroke::new(2.0 / 2.0, Color32::GREEN));
+
+            // paint the tail
+            let tail_pos = to_panel_pos(self.tail);
+            painter.circle_stroke(tail_pos, 3.0 / 2.0, Stroke::new(2.0 / 2.0, Color32::YELLOW));
+
+            // paint an arrow from head to tail
+            painter.arrow(
+                tail_pos,
+                head_pos - tail_pos,
+                Stroke::new(2.0 / 2.0, Color32::YELLOW),
+            );
         });
+        ctx.request_repaint();
     }
 }
 
