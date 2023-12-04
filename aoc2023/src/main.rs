7baba2b515edf4dd88048855321cdfbe8ddc3efc
Mikkel <Mikkel123sej@gmail.com>
2023-12-04 11:38:44 +0100

    did day 4 part 1

--- aoc2023/src/main.rs
+++ aoc2023/src/main.rs
@@ -3,6 +3,7 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 fn main() {
     // let day_input = std::fs::read_to_string("input/day_1.txt").unwrap();
@@ -11,7 +12,10 @@ fn main() {
     // let day_input = std::fs::read_to_string("input/day_2.txt").unwrap();
     // println!("{}", day_2::part_1(&day_input));
     // println!("{}", day_2::part_2(&day_input));
-    let day_input = std::fs::read_to_string("input/day_3.txt").unwrap();
-    println!("{}", day_3::part_1(&day_input));
-    // println!("{}", day_1::part_2(&day_input));
+    // let day_input = std::fs::read_to_string("input/day_3.txt").unwrap();
+    // println!("{}", day_3::part_1(&day_input));
+    // println!("{}", day_3::part_2(&day_input));
+    let day_input = std::fs::read_to_string("input/day_4.txt").unwrap();
+    println!("{}", day_4::part_1(&day_input));
+    // println!("{}", day_4::part_2(&day_input));
 }
