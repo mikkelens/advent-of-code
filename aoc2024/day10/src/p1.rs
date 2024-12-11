89d6547d353c791f6a1ea9bdb34b2270abe1b74e
Mikkel <mikkelfss@gmail.com>
2024-12-11 11:01:53 +0100

    d10 & d11 infra

---
+++ aoc2024/day10/src/p1.rs
@@ -0,0 +1,26 @@
+#![doc = include_str!("../p1.md")]
+
+#[allow(unused_imports)]
+use winnow::{
+	ascii::*,
+	combinator::*,
+	error::*,
+	token::*,
+	{PResult, Parser}
+};
+
+fn main() {
+	util::DayInput::find::<10>().solve_with(solve);
+}
+
+fn solve(input: impl AsRef<str>) -> u64 {
+	todo!()
+}
+#[cfg(test)]
+mod tests {
+
+	#[test]
+	fn input_solvable() {
+		assert_eq!(super::solve(include_str!("../../inputs/10")), 0);
+	}
+}
