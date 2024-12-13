d12ede18afe1b1e03f39d0a6eec0ec67afd2e724
Mikkel <Mikkel123sej@gmail.com>
2024-12-13 05:44:43 +0100

    work on d12p1, no tests pass

---
+++ aoc2024/day12/src/p2.rs
@@ -0,0 +1,35 @@
+//#![doc = include_str!("../p2.md")]
+
+#[allow(unused_imports)]
+use winnow::{
+    ascii::*,
+    combinator::*,
+    error::*,
+    prelude::*,
+    stream::*,
+    token::*,
+    {PResult, Parser},
+};
+
+fn main() {
+    util::DayInput::find::<12>().solve_with(solve);
+}
+
+fn solve(input: impl AsRef<str>) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn trailhead_score_sum_examples() {
+        todo!();
+        assert_eq!(super::solve(include_str!("EXAMPLE")), todo!());
+    }
+
+    #[ignore]
+    #[test]
+    fn input_solvable() {
+        assert_eq!(super::solve(include_str!("../../inputs/10")), 607);
+    }
+}
